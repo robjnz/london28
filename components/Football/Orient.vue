9d584cebe45431941d91b6bcab24e2c80a22c33f
robin harris <robjnz@outlook.com>
2019-09-27 22:02:43 +0100

    update football

--- components/Football/Orient.vue
+++ components/Football/Orient.vue
@@ -1,11 +1,31 @@
 <template>
   <b-card bg-variant="dark" text-variant="white" title= "Leyton Orient F.C." align ="center">
 <a href="https://kutt.it/orient-fc" target="_blank"><b-img src="https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_200,w_300/v1544943996/leyton_orient_wltsdq.jpg" fluid-grow alt="Leyton Orient F.C."/></a>
-<p class="title-color">
+<p class="asian">
 
-
-<br>
-
+Sat 28 Sept 2019 @ 3pm <br>V<br>Port Vale<br>
+<!--
+Sat 12 Oct 2019 @ 3pm <br>V<br>Walsall<br>
+Sat 26 Oct 2019 @ 3pm <br>V<br>Carlisle United<br>
+Tues 5 Nov 2019 @ 7.45pm <br>V<br>Brighton & Hove Albion<br>
+Sat 16 Nov 2019 @ 3pm <br>V<br>Scunthorpe United<br>
+Sat 23 Nov 2019 @ 3pm <br>V<br>Forest Green Rovers<br>
+Sat 14 Dec 2019 @ 3pm <br>V<br>Bradford City<br>
+Thurs 26 Dec 2019 @ 1pm <br>V<br> Colchester United<br>
+Sat 4 Jan 2020 @ 3pm <br>V<br>Northamton Town<br>
+Sat 11 Jan 2020 @ 3pm <br>V<br>Grimsby Town<br>
+Sat 25 Jan 2020 @ 3pm <br>V<br>NewportCounty<br>
+Sat 8 Feb 2020 @ 3pm <br>V<br>Macclesfield Town<br>
+Tues 11 Feb 2020 @ 7.45pm<br>V<br>Mansfield Town<br>
+Sat 22 Feb 2020 @ 3pm <br>V<br>Oldham Athletic<br>
+Sat 7 March 2020 @ 3pm <br>V<br>Cambridge United<br>
+Tues 17 March 2020 @ 7.45pm <br>V<br>Plymouth Argyle<br>
+Sat 28 March 2020 @ 3pm <br>V<br>Morecambe<br>
+Mon 13 April 2020 @ 3pm <br>V<br>Salford City<br>
+Sat 25 April 2020 @ 3pm <br>V<br>Exeter City <br>
+-->
 </p>
+    <b-button href="https://kutt.it/orient-fc" target="_blank" variant="info">open</b-button>
+
 </b-card>
 </template>
