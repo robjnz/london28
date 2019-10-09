38c8f3626455f6975f31f52a06fe63e4d6a1d5a1
robin harris <robjnz@outlook.com>
2019-10-09 22:40:35 +0100

    todays updates

--- components/Football/Walthamstow.vue
+++ components/Football/Walthamstow.vue
@@ -1,10 +1,25 @@
 <template>
   <b-card bg-variant="dark" text-variant="white" title= "Walthamstow F.C." align ="center">
 <a href="https://kutt.it/walthamstow-fc" target="_blank"><b-img src="https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_200,w_300/v1544943990/walthamstow_fc_wjphs2.jpg" fluid-grow alt="Walthamstow F.C."/></a>
-<p class="title-color">
-
-  <br>
-
+<p class="asian">
+Tues Oct 22 2019 @ 7.45pm <br>V<br>Tower Hamlets<br>
+<!--
+Sat Nov 9 2019 @ 3pm <br>V<br>Saffron Walden<br>
+Tues Nov 12 2019 @ 7.45pm <br>V<br>Woodford Town<br>
+Tues Nov 19 2019 @ 7.45pm <br>V<br>Takeley<br>
+Sat Nov 23 2019 @ 3pm <br>V<br> Southend Manor<br>
+Sat Nov 30 2019 @ 3pm <br>V<br> Hashtag United<br>
+Tues Dec 3 2019 @ 7.45pm <br>V<br> Hoddesdon Town<br>
+Sat Jan 4 2020 @ 3pm <br>V<br>Clapton<br>
+Sat Feb 8 2020 @ 3pm <br>V<br>Sawbridgeworth Town<br>
+Sat Feb 29 2020 @ 3pm <br>V<br>Enfield<br>
+Tues March 17 2020 @ 7.45pm <br>V<br>West Essex<br>
+Sat March 28 2020 @ 3pm <br>V<br>St Margaretsbury<br>
+Sat April 11 2020 @ 3pm <br>V<br>Redbridge<br>
+April 25 2020 @ 3pm <br>V<br>Sporting Bengal United<br>
+-->
 </p>
+    <b-button href="https://kutt.it/walthamstow-fc" target="_blank" variant="info">open</b-button>
+
 </b-card>
 </template>
