610795e853ab54e6796d32405d6d4f76e64a8f85
robin harris <robjnz@outlook.com>
2019-10-12 13:24:47 +0100

    add instagram curator

--- pages/today.vue
+++ pages/today.vue
@@ -11,26 +11,42 @@ class="mb-3">
 <a class="twitter-timeline" data-width="320" data-height="600" data-theme="dark" href="https://twitter.com/londoneast3?ref_src=twsrc%5Etfw">Tweets by londoneast3</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </b-card>
 
-
 <b-card bg-variant="dark" text-variant="white" title= "">
-<b-embed type="iframe"
-          aspect = "16by9"
-          src    = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Flondoneastlife-104681190948236%2F&tabs=timeline&width=320&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width = "360" height = "600" style = "border:none;overflow:hidden" scrolling = "no" frameborder = "0" allowTransparency = "true" allow = "encrypted-media"
-          allowfullscreen
-  ></b-embed>
-</b-card>
 
+</b-card>
 <b-card bg-variant="dark" text-variant="white" title= "">
-<!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/5a1774d4f53f53dc91ef8d91c85e019c.html" scrolling="yes" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
-</b-card>
 
-</b-card-group>
+</b-card>
+    </b-card-group>
+
+ <b-card-group deck
+class="mb-3">
+<b-card bg-variant="dark" text-variant="white" title= "">
+<!-- Place <div> tag where you want the feed to appear -->
+<div id="curator-feed-default-layout"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>
+<!-- The Javascript can be moved to the end of the html page before the </body> tag -->
+<script type="text/javascript">
+/* curator-feed-default-layout */
+(function(){
+var i,     e, d = document, s = "script";i = d.createElement("script");i.async = 1;
+    i.src = "https://cdn.curator.io/published/df50401c-f6ed-4798-bef7-f3311a5124d0.js";
+    e     = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+})();
+</script>
+</b-card>
+ </b-card-group>
+
+
+  <b-card-group deck
+class="mb-3">
+<b-card bg-variant="dark" text-variant="white" title= "">
 <b-embed type="iframe"
           aspect = "16by9"
           src    = "https://www.google.com/maps/d/embed?mid=1F9WADbgsmZ44JzTDdiOnHzyfux3uOFxI" width = "640" height = "480"
           allowfullscreen
   ></b-embed>
-
+</b-card>
+ </b-card-group>
 
 </b-container>
       </section>
