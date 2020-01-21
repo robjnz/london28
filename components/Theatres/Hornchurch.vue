7f2e0124c0056f9ae8341a539a081029271363eb
robin harris <robjnz@outlook.com>
2020-01-21 18:43:12 +0000

    update threatre

--- components/Theatres/Hornchurch.vue
+++ components/Theatres/Hornchurch.vue
@@ -1,19 +1,22 @@
 <template>
-  <b-card bg-variant="dark" text-variant="white" title= "Robin Hood" align ="center">
-    <br>
-    <b-img src="https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_200,w_300/v1573899326/Robin_Hood_zsqgzt.jpg" fluid-grow alt="Robin Hood poster"/>
-
+  <b-card bg-variant="dark" text-variant="white" title= "Ladies Of Laughter" align ="center">
+    <br><br>
+<!--<b-img src="https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_200,w_300/v1573899326/Robin_Hood_zsqgzt.jpg" fluid-grow alt="Robin Hood poster"/>
+-->
+<b-embed type="iframe"
+          aspect = "16by9"
+          src    = "https://www.youtube.com/embed/G_o-VNGLsp8"
+          allowfullscreen
+  ></b-embed>
   <p class="title-color">
     Queen's Theatre Hornchurch<br>
-    Thurs 28 Nov 2019 - Sun 5 Jan 2020
+    Sat 25 Jan 2020 @ 7.30 pm
     </p>
 
 <p class="asian">
 </p>
 <div class="text-center my-3">
-  <b-button block variant="success" v-b-tooltip.hover title="Deep in the heart of his foreboding Castle, the dastardly Sheriff of Nottingham is taxing the poor to pile up his gold, whilst imprisoning brave Maid Marian and her hilarious Nurse Nellie.
-
-But the Sheriff hasn’t banked on our arrow slinging hero Robin emerging from Sherwood Forest to target the hand of Marion and outwit the villain’s plans. Can Robin save the day with a little help from his band of Friar Tuck and plucky Merry Folk?">Description Read Me</b-button>
+  <b-button block variant="success" v-b-tooltip.hover title="Noreen Khan (BBC Asian Network) as she hosts a night of comedy with some of the UK’s favourite female acts.">Description Read Me</b-button>
 </div>
 <p class="labour">
 </p>
--- components/Theatres/Ilford.vue
+++ components/Theatres/Ilford.vue
@@ -1,17 +1,22 @@
 <template>
-  <b-card bg-variant="dark" text-variant="white" title= "Sleeping Beauty" align ="center">
-    <br>
-      <b-img src="https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_200,w_300/v1573461464/sleepingilford_rhabha.jpg" fluid-grow alt="Sleeping Beauty poster"/>
-
+  <b-card bg-variant="dark" text-variant="white" title= "Histories of the Unexpected" align ="center">
+    <br><br>
+      <!--<b-img src="https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_200,w_300/v1573461464/sleepingilford_rhabha.jpg" fluid-grow alt="Histories of the Unexpected poster"/>
+-->
+<b-embed type="iframe"
+          aspect = "16by9"
+          src    = "https://www.youtube.com/embed/0BJNBcm2fUQ"
+          allowfullscreen
+  ></b-embed>
   <p class="title-color">
     Kenneth More Theatre Ilford<br>
-    Sat 14 Dec 2019 - Sun 12 Jan 2020
+    Tues 21 - Thurs 30 Jan 2020
 </p>
 
 <p class="asian">
 </p>
 <div class="text-center my-3">
-  <b-button block variant="success" v-b-tooltip.hover title="Princess Aurora has lived a happy childhood as she prepares to celebrate her 18th birthday, she is unaware of the curse placed upon at birth by the wicked fairy Carabosse.  Intrigued by a mysterious old spinning wheel, Aurora pricks her finger and falls asleep... Now it's up to her best friend and court jester Wally, his mother Dame Dolly and the magical Fairy Starlight to aid Prince Percy on his quest to rescue the Princess from her terrible fate.  Only 100 years and a fearsome dragon stand in the way...">Description Read Me</b-button>
+  <b-button block variant="success" v-b-tooltip.hover title="Histories of the Unexpected LIVE! is an original, exciting multi-period show that demonstrates how everything has a history - even the most unexpected of subjects.">Description Read Me</b-button>
 </div>
 <p class="labour">
 </p>
